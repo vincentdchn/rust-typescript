0c652b817aa29e19c9233910082e0735acc14736
vincentdchn <vincentduchene.m@gmail.com>
2023-08-27 15:05:02 +0200

    feat: borrow checker - 2

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ struct Item {
     count: usize,
 }
 
-fn add_one(item: &mut Item) {
-    item.count += 1;
+fn print_all(items: &Vec<Item>) {
+    for item in items {
+        println!("{:?}", item);
+    }
 }
-fn main() {
-    let mut item = Item { count: 1 };
-    println!("{:?}", item);
 
-    add_one(&mut item);
-    println!("{:?}", item);
+fn main() {
+    let mut items = vec![Item { count: 1 }];
+    let first = items.first_mut();
+    println!("{:?}", first);
+    print_all(&mut items);
 }
